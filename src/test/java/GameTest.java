ab2ffa5852ea562290a6d6d2e7b7e524de602a0e
Irene Papakonstantinou <eirhnh@gmail.com>
2012-11-07 13:42:27 +0000

    rename the tests

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    public void rockDefeatsScissors() {
+    public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesRockThenPlayer2Wins() {
         Game game = new Game();
         Result result = game.player1(Move.SCISSORS).player2(Move.ROCK).play();
         Assertions.assertThat(result.isDraw()).isFalse();
@@ -12,7 +12,7 @@ public class GameTest {
     }
 
     @Test
-    public void scissorsDefeatPaper() {
+    public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesPaperThenPlayer1Wins() {
         Game game = new Game();
         Result result = game.player1(Move.SCISSORS).player2(Move.PAPER).play();
         Assertions.assertThat(result.isDraw()).isFalse();
