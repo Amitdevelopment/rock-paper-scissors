f041d8bbc5b41c4b317b3c69bf8552699a347911
Irene Papakonstantinou <eirhnh@gmail.com>
2012-11-07 14:05:45 +0000

    add test for when both players choose Rock there should be a draw

--- src/main/java/Rules.java
+++ src/main/java/Rules.java
@@ -1,5 +1,6 @@
 public class Rules {
-    public static int compareMoves(Move player1Move, Move player2Move) {
+    public static int compareMoves (Move player1Move, Move player2Move) {
+        if (player1Move == player2Move) { return 0; }
         return (player2Move == Move.ROCK) ? -1 : 1;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesRockThenPlayer2Wins() {
+    public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesRockThenPlayer2Wins () {
         Game game = new Game();
         Result result = game.player1(Move.SCISSORS).player2(Move.ROCK).play();
         Assertions.assertThat(result.isDraw()).isFalse();
@@ -12,10 +12,17 @@ public class GameTest {
     }
 
     @Test
-    public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesPaperThenPlayer1Wins() {
+    public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesPaperThenPlayer1Wins () {
         Game game = new Game();
         Result result = game.player1(Move.SCISSORS).player2(Move.PAPER).play();
         Assertions.assertThat(result.isDraw()).isFalse();
         Assertions.assertThat(result.getWinner()).isEqualTo(Player.PLAYER1);
+    }
+
+    @Test
+    public void whenBothPlayersChooseRockThereIsADraw () {
+        Game game = new Game();
+        Result result = game.player1(Move.ROCK).player2(Move.ROCK).play();
+        Assertions.assertThat(result.isDraw()).isTrue();
     }
 }
