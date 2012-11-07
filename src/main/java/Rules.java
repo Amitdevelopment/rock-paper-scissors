4d9ad42bcf831ac0803acab6b39619bc17f5bc23
Irene Papakonstantinou <eirhnh@gmail.com>
2012-11-07 15:17:36 +0000

    two more tests with the same moves but played  by the opposite players

--- src/main/java/Rules.java
+++ src/main/java/Rules.java
@@ -1,6 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rules {
-    public static int compareMoves (Move player1Move, Move player2Move) {
-        if (player1Move == player2Move) { return 0; }
-        return (player2Move == Move.ROCK) ? -1 : 1;
+
+    private static Map<Move, Integer> positions = new HashMap<Move, Integer>();
+    private static final Integer SIZE = Move.values().length;
+
+    //if we view this as a cyclical array, each item defeats its right neighbor and is defeated by its left neighbor
+    static {
+        positions.put(Move.PAPER, 1);
+        positions.put(Move.ROCK, 2);
+        positions.put(Move.SCISSORS, 3);
+    }
+
+    public static int compareMoves (Move move1, Move move2) {
+        if (move1 == move2) { return 0; }
+        return isToTheLeftOf(move1, move2) ? 1 : -1;
+    }
+
+    private static boolean isToTheLeftOf (final Move move1, final Move move2) {
+        return positions.get(move1) % SIZE == (positions.get(move2) + SIZE - 1) % SIZE;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -12,11 +12,27 @@ public class GameTest {
     }
 
     @Test
+    public void whenPlayer1ChoosesRockAndPlayer2ChoosesScissorsThenPlayer1Wins () {
+        Game game = new Game();
+        Result result = game.player1(Move.ROCK).player2(Move.SCISSORS).play();
+        Assertions.assertThat(result.hasWinner()).isTrue();
+        Assertions.assertThat(result.getWinner()).isEqualTo(Player.PLAYER1);
+    }
+
+    @Test
     public void whenPlayer1ChoosesScissorsAndPlayer2ChoosesPaperThenPlayer1Wins () {
         Game game = new Game();
         Result result = game.player1(Move.SCISSORS).player2(Move.PAPER).play();
         Assertions.assertThat(result.hasWinner()).isTrue();
         Assertions.assertThat(result.getWinner()).isEqualTo(Player.PLAYER1);
+    }
+
+    @Test
+    public void whenPlayer1ChoosesPaperAndPlayer2ChoosesScissorsThenPlayer2Wins () {
+        Game game = new Game();
+        Result result = game.player1(Move.PAPER).player2(Move.SCISSORS).play();
+        Assertions.assertThat(result.hasWinner()).isTrue();
+        Assertions.assertThat(result.getWinner()).isEqualTo(Player.PLAYER2);
     }
 
     @Test
